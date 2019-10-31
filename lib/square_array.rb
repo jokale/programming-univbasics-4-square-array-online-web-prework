40a51d97f7557b8d3fb2f616b59164c88cf4210f
Joanna E K <kalemajoanna@gmail.com>
2019-10-31 18:17:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,14 @@
 def square_array(array)
   new_array = []
   counter = 0
+<<<<<<< HEAD
   while counter < array.length do 
 		new_array << array[counter] ** 2
  counter +=1
  end 
  new_array
  end 
+=======
+ end
+>>>>>>> 0ef05d3db3c75e5257f1e3eba00ebd86088ce9d8
     
